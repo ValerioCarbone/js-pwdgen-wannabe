44441dcd9b60678323e5e5c0906baa295bda04e3
ValerioCarbone <valeriocarbone10@gmail.com>
2023-10-03 16:46:26 +0200

    ADD js + html

---
+++ README.md
@@ -0,0 +1,26 @@
+# GENERATORE DI PASSWORD
+
+ - Chiedere all'utente il suo nome
+   - Dichiarare una variabile
+   - Assegnare alla variabile il risultato del prompt con cui chiediamo all'utente il suo nome
+
+ - Chiedere all'utente il suo cognome
+   - Dichiarare una variabile
+   - Assegnare alla variabile il risultato del prompt con cui chiediamo all'utente il suo cognome
+
+ - Chiedere all'utente il suo colore preferito
+   - Dichiarare una variabile
+   - Assegnare alla variabile il risultato del prompt con cui chiediamo all'utente il suo colore preferito
+
+ - Generare un numero casuale
+   - Dichiarare una variabile 
+   - Assegnare alla variabile il risultato di un generatore casuale di numeri
+
+ - Generare una variabile che concatena le quattro variabili precedenti in un'unica stringa
+   - Dichiarare una variabile
+   - Assegnare alla variabile il risultato della concatenazione delle quattro variabili precedenti
+
+ - Mostrare all'utente il risultato della password generata
+   - Inserire nell'html un elemento che contenga un testo
+   - Richiamare nel js l'id dell'elemento
+   - Modificare tale elemento con il risultato della variabile di concatenazione
---
+++ js/app.js
@@ -0,0 +1,40 @@
+// # GENERATORE DI PASSWORD
+
+//  - Chiedere all'utente il suo nome
+//    - Dichiarare una variabile
+//    - Assegnare alla variabile il risultato del prompt con cui chiediamo all'utente il suo nome
+const userName = prompt('Ciao, qual è il tuo nome?')
+console.log(userName)
+//  - Chiedere all'utente il suo cognome
+//    - Dichiarare una variabile
+//    - Assegnare alla variabile il risultato del prompt con cui chiediamo all'utente il suo cognome
+const userSurname = prompt('Qual è il tuo cognome?')
+console.log(userSurname)
+
+// - Chiedere all'utente il suo colore preferito
+// - Dichiarare una variabile
+// - Assegnare alla variabile il risultato del prompt con cui chiediamo all'utente il suo colore preferito
+const userFavColor = prompt('Qual è il tuo colore preferito?')
+console.log(userFavColor)
+//  - Generare un numero casuale
+//    - Dichiarare una variabile 
+//    - Assegnare alla variabile il risultato di un generatore casuale di numeri
+
+const randomNumber = getRandomInt(999)
+console.log(randomNumber)
+
+//  - Generare una variabile che concatena le quattro variabili precedenti in un'unica stringa
+//    - Dichiarare una variabile
+//    - Assegnare alla variabile il risultato della concatenazione delle quattro variabili precedenti
+const passwordGenerated = (userName + userSurname + userFavColor + randomNumber)
+console.log(passwordGenerated)
+//  - Mostrare all'utente il risultato della password generata
+//    - Inserire nell'html un elemento che contenga un testo
+//    - Richiamare nel js l'id dell'elemento
+//    - Modificare tale elemento con il risultato della variabile di concatenazione
+const yourPassword = document.getElementById('your_password')
+yourPassword.innerHTML = passwordGenerated
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
